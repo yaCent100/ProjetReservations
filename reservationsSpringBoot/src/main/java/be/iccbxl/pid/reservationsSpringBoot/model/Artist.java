9ceef7ca75d2f8428f8cb8f2dc4ebd515fc0b879
yassin <iallaleny@gmail.com>
2023-10-15 15:24:45 +0200

    "Relation ManyToMany between Artist & Type"

--- reservationsSpringBoot/src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Artist.java
+++ reservationsSpringBoot/src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Artist.java
@@ -5,6 +5,9 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
 @Entity
@@ -15,6 +18,27 @@ public class Artist {
     private Long id;
     private String firstname;
     private String lastname;
+
+    @ManyToMany(mappedBy = "artists")
+    private List<Type> types = new ArrayList<>();
+
+    public Artist addType(Type type) {
+        if(!this.types.contains(type)) {
+            this.types.add(type);
+            type.addArtist(this);
+        }
+
+        return this;
+    }
+
+    public Artist removeType(Type type) {
+        if(this.types.contains(type)) {
+            this.types.remove(type);
+            type.getArtists().remove(this);
+        }
+
+        return this;
+    }
 
 
 }
--- reservationsSpringBoot/src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Type.java
+++ reservationsSpringBoot/src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Type.java
@@ -5,6 +5,9 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
 @Entity
@@ -14,4 +17,31 @@ public class Type {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String type;
+
+    @ManyToMany
+    @JoinTable(
+            name = "artist_type",
+            joinColumns = @JoinColumn(name = "type_id"),
+            inverseJoinColumns = @JoinColumn(name = "artist_id"))
+    private List<Artist> artists = new ArrayList<>();
+
+
+    public Type addArtist(Artist artist) {
+        if(!this.artists.contains(artist)) {
+            this.artists.add(artist);
+            artist.addType(this);
+        }
+
+        return this;
+    }
+
+    public Type removeType(Artist artist) {
+        if(this.artists.contains(artist)) {
+            this.artists.remove(artist);
+            artist.getTypes().remove(this);
+        }
+
+        return this;
+    }
+
 }
