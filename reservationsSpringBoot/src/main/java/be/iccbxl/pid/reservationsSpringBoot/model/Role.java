5a46cd7af656d6a4a79b6f252dc6d074ca29c96a
yassin <iallaleny@gmail.com>
2023-10-15 19:52:32 +0200

    "Update templates Show (added artists’ list)"

--- reservationsSpringBoot/src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Role.java
+++ reservationsSpringBoot/src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Role.java
@@ -5,6 +5,9 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
 @Entity
@@ -14,5 +17,35 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String role;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users = new ArrayList<>();
+
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public Role addUser(User user) {
+        if(!this.users.contains(user)) {
+            this.users.add(user);
+            user.addRole(this);
+        }
+
+        return this;
+    }
+
+    public Role removeUser(User user) {
+        if(this.users.contains(user)) {
+            this.users.remove(user);
+            user.getRoles().remove(this);
+        }
+
+        return this;
+    }
 
 }
