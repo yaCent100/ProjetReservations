f1776e809d12db4f7f918f6f059b3ccbb12316e8
yassin <iallaleny@gmail.com>
2023-10-21 15:39:21 +0200

    test

--- src/main/java/be/iccbxl/pid/reservationsSpringBoot/controller/AdminController.java
+++ src/main/java/be/iccbxl/pid/reservationsSpringBoot/controller/AdminController.java
@@ -6,15 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import be.iccbxl.pid.reservationsSpringBoot.model.Show;
+import be.iccbxl.pid.reservationsSpringBoot.model.User;
 import be.iccbxl.pid.reservationsSpringBoot.service.ShowService;
+import be.iccbxl.pid.reservationsSpringBoot.service.UserService;
 
 @Controller
 public class AdminController {
 	
 	@Autowired
 	ShowService showService;
+	
+	@Autowired
+	UserService userService;
 	
 	
 	
@@ -33,6 +39,16 @@ public class AdminController {
 	    model.addAttribute("title", "Liste des spectacles");
 
 	    return "admin/shows";
+	}
+	
+	@GetMapping("/admin/users")
+	public String users(Model model) {
+		List<User> users = userService.getAllUsers();
+		
+		model.addAttribute("users", users);
+		model.addAttribute("title", "Utilisateurs");
+		
+		return "admin/user";
 	}
 	
 	
